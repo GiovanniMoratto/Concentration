b31d73c949cb7124ad83b2a36bb58a853ca5fc5d
giovanni.moratto <giovanni.moratto@zup.com.br>
2021-09-16 15:50:09 -0300

    Função de virada do cartão criada

--- concentration-game/concentration-game/ConcentrationViewController.swift
+++ concentration-game/concentration-game/ConcentrationViewController.swift
@@ -15,8 +15,23 @@ class ConcentrationViewController: UIViewController {
     
     /// Método para capturar ação de toque no card
     @IBAction func touchCard(_ sender: UIButton) {
-        print("agh! a ghost")
+        flipCard(withEmoji: "👻", on: sender)
     }
     
+    /// Função para gerar efeito visual de virada  no card
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        /// Verifica se o titulo atual do botão é igual ao emoji
+        if button.currentTitle == emoji {
+            /// Apaga emoji
+            button.setTitle("", for: UIControl.State.normal)
+            /// Defini a cor traseira do card
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+        } else {
+            /// Inseri emoji
+            button.setTitle(emoji, for: UIControl.State.normal)
+            /// Defini a cor frontal do card
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
 
