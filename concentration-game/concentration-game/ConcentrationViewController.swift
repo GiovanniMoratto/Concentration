2f50eaf411319fbfe61786e30f06677c48f29973
giovanni.moratto <giovanni.moratto@zup.com.br>
2021-09-16 15:36:28 -0300

    Imprimindo resposta de toque no console

---
+++ concentration-game/concentration-game/ConcentrationViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  concentration-game
+//
+//  Created by Giovanni Vicentin Moratto on 16/09/21.
+//
+
+import UIKit
+
+///
+/// View-controller do Concentration Game
+///
+class ConcentrationViewController: UIViewController {
+    
+    
+    /// Método para capturar ação de toque no card
+    @IBAction func touchCard(_ sender: UIButton) {
+        print("agh! a ghost")
+    }
+    
+}
+
